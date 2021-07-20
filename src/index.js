5b8bdc677ddd67b996c479311d6df2e6183e9367
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-07-20 19:39:21 +0300

    разделила код по разным файлам js, добавила комментарии

--- src/index.js
+++ src/index.js
@@ -1,108 +1,12 @@
-// import { menu } from './js/menu';
-import menuCardsTpl from './templates/menu-cards.hbs';
-import cards from './menu.json';
+import { menu } from './js/menu';
 import './sass/main.scss';
+// import menuCardsTpl from './templates/menu-cards.hbs';
+// import cards from './menu.json';
 
-// menu();
+menu();
 
-const refs = {
-  ulMenuRef: document.querySelector('.js-menu'),
-  checkboxRef: document.getElementById('theme-switch-toggle'),
-};
-
-// функция создания карточки меню
-function createMenuCadrs(cards) {
-  return menuCardsTpl(cards);
-}
-refs.ulMenuRef.insertAdjacentHTML('beforeend', createMenuCadrs(cards));
-
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-
-let theme = localStorage.getItem('theme') || Theme.LIGHT;
-document.body.classList.add(theme);
-
-if (theme === Theme.DARK) {
-  refs.checkboxRef.checked = true;
-}
-
-// функция смены темы на светлая/темная
-function menuTheme() {
-  // if (!document.body.getAttribute('class') || document.body.classList.contains(Theme.LIGHT)) {
-  //   document.body.classList.add(Theme.DARK);
-  //   document.body.classList.remove(Theme.LIGHT);
-  // } else {
-  //   document.body.classList.add(Theme.LIGHT);
-  //   document.body.classList.remove(Theme.DARK);
-  // }
-
-  saveItem();
-}
-refs.checkboxRef.addEventListener('change', switchTheme);
-
-function switchTheme() {
-  document.body.classList.toggle(Theme.DARK);
-  document.body.classList.toggle(Theme.LIGHT);
-
-  saveItem();
-}
-
-// функция для localStorage-сохр при перезагрузки стр в localStorage
-function saveItem() {
-  theme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
-  localStorage.setItem('theme', theme);
-}
-
-// feature detection js(выявление возможностей браузера) about lazyload
-if ('loading' in HTMLImageElement.prototype) {
-  console.log('Браузер поддерживает lazyload');
-  addSrcAttrToLazyImages();
-} else {
-  console.log('Браузер НЕ поддерживает lazyload');
-  addLazySizesScript();
-}
-
-// функция lazyload с поддержкой нативной и библиотеки
-function addSrcAttrToLazyImages() {
-  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
-
-  // ленивая загрузка - проверка загружаемости картинок
-  lazyImages.forEach(image => image.addEventListener('load', onImageLoaded, { once: true }));
-
-  function onImageLoaded() {
-    console.log('Картинка загрузилась');
-  }
-
-  lazyImages.forEach(img => {
-    img.src = img.dataset.src;
-  });
-}
-function addLazySizesScript() {
-  const script = document.createElement('script');
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
-  script.integrity =
-    'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
-  script.crossorigin = 'anonymous';
-  script.referrerpolicy = 'no-referrer';
-
-  document.body.appendChild(script);
-}
 // ----------------------------------------------------------------------
-// вопросы
-// 1.куда деть айди из меню.джейсон??? где его использовать?
-// 2. почему не могу достучаться до 1го эл массива ингридиентс
-// если {{ingredients[0]}} - ошибка
-// 3. почему document.body.classList.add('light-theme');а не - body.classList.add('light-theme');. зачем писать тут документ?
-// 5. console.log(JSON.parse(cards));-почему ошибка? почему при стрингафай все ок?
-// 6. почему не работает с тернарником смена темы?
-//     !document.body.getAttribute('class') || document.body.classList.contains(Theme.LIGHT)
-//     ? document.body.classList.toggle(Theme.DARK)
-//     : document.body.classList.toggle(Theme.LIGHT);
-// 7. если добавляется эл динамически, я его не могу найти, что-то в него добавить?
-// ----------------------------------------------------------------------
+
 // сделать
 // 1. просмотреть еще раз ленивую загрузку
-// 2. разбить джс по файлам, а не все в index.js
-// 3.  удалить стайл.цсс
+// 2. подумать еще варианты с localStorage
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,86 @@
-// import menuCardsTpl from '../templates/menu-cards.hbs';
-// import cards from '../menu.json';
+import menuCardsTpl from '../templates/menu-cards.hbs';
+import cards from '../menu.json';
 
-// export const menu = () => {
+export const menu = () => {
+  const ulMenuRef = document.querySelector('.js-menu');
+  const checkboxRef = document.getElementById('theme-switch-toggle');
 
-// };
+  const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+  // функция создания карточки меню
+  function createMenuCadrs(cards) {
+    return menuCardsTpl(cards);
+  }
+  ulMenuRef.insertAdjacentHTML('beforeend', createMenuCadrs(cards));
+
+  // установить у боди светлую тему по умолчанию
+  // или можно было в шаблоне добавить <body class="light-theme"> - не работает, почему?
+  let theme = localStorage.getItem('theme') || Theme.LIGHT;
+  document.body.classList.add(theme);
+
+  // если тема темная, при перезагрузки стр поставить ползунок вправо как выбранный
+  if (theme === Theme.DARK) {
+    checkboxRef.checked = true;
+  }
+
+  // функция смены темы на светлая/темная
+  function toggleTheme() {
+    // if (!document.body.getAttribute('class') || document.body.classList.contains(Theme.LIGHT)) {
+    //   document.body.classList.add(Theme.DARK);
+    //   document.body.classList.remove(Theme.LIGHT);
+    // } else {
+    //   document.body.classList.add(Theme.LIGHT);
+    //   document.body.classList.remove(Theme.DARK);
+    // }
+
+    document.body.classList.toggle(Theme.DARK);
+    document.body.classList.toggle(Theme.LIGHT);
+
+    saveItem();
+  }
+  checkboxRef.addEventListener('change', toggleTheme);
+
+  // функция для localStorage-сохр при перезагрузки стр в localStorage
+  function saveItem() {
+    theme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+    localStorage.setItem('theme', theme);
+  }
+
+  // feature detection js(выявление возможностей браузера) about lazyload
+  if ('loading' in HTMLImageElement.prototype) {
+    console.log('Браузер поддерживает lazyload');
+    addSrcAttrToLazyImages();
+  } else {
+    console.log('Браузер НЕ поддерживает lazyload');
+    addLazySizesScript();
+  }
+
+  // функция lazyload с поддержкой нативной и библиотеки
+  function addSrcAttrToLazyImages() {
+    const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+    // ленивая загрузка - проверка загружаемости картинок
+    lazyImages.forEach(image => image.addEventListener('load', onImageLoaded, { once: true }));
+
+    function onImageLoaded() {
+      console.log('Картинка загрузилась');
+    }
+
+    lazyImages.forEach(img => {
+      img.src = img.dataset.src;
+    });
+  }
+  function addLazySizesScript() {
+    const script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+    script.integrity =
+      'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
+    script.crossorigin = 'anonymous';
+    script.referrerpolicy = 'no-referrer';
+
+    document.body.appendChild(script);
+  }
+};
